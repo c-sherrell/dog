10ca772e0a0fbca713db5a2d5a65ed26511e832f
Christopher Sherrell <114314208+c-sherrell@users.noreply.github.com>
2024-02-25 20:05:28 -0800

    Add files via upload

---
+++ dog test.py
@@ -0,0 +1,19 @@
+import requests
+from PIL import Image
+import io
+import dog
+
+dog.get_img()
+
+print("Endpoint Hit:  http://127.0.0.1:5000/get/dog/image")
+img_route = 'http://127.0.0.1:5000/get/dog/image'
+img_content = requests.get(img_route).content
+img_content = io.BytesIO(img_content)
+img = Image.open(img_content)
+img.show()
+
+print("Endpoint Hit:  http://127.0.0.1:5000/get/dog/fact")
+fact_route = 'http://127.0.0.1:5000/get/dog/fact'
+fact = requests.get(fact_route).content
+fact = fact.decode('utf-8')
+print(fact)
